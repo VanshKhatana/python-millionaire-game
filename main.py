0ad52c588b684f81d2c586cc18400bedab663aac
VanshKhatana <vansh2003khatana@gmail.com>
2026-06-20 21:33:36 +0530

    update

--- main.py
+++ main.py
@@ -11,18 +11,27 @@ questions = [
     ["Which ocean is the largest?", "Atlantic ocean", "Indian ocean", "Arctic ocean", "Pacific ocean", 4],
 ]
 
+prizes = [100000, 320000, 400000, 450000, 500000, 1000000, 2000000, 3000000, 4000000, 5000000]
+sum = 0
+i = 0
+
 for question in questions:
+    
     print(question[0])
-    print(f"a. {question[1]}")
-    print(f"b. {question[2]}")
-    print(f"c. {question[3]}")
-    print(f"d. {question[4]}")
+    print(f"1. {question[1]}")
+    print(f"2. {question[2]}")
+    print(f"3. {question[3]}")
+    print(f"4. {question[4]}")
 
     #Check weather the answer is correct or not
-    a = int(input("Enter Your answer. 1 for a, 2 for b, 3 for c, 4 for d, 5 for e: "))
+    a = int(input("Enter Your answer: "))
     if (question[5]==a):
-        print("Correct Answer")
+        print(f"\nYou won {prizes[i]} for the correct Answer")
     else:
         print(f"Incorrect, the correct answer was {question[5]}")
         print("Better luck next time!")
-        break 
+        break
+
+    sum += prizes[i]
+    print(f"You total amount till now is {sum}\n")
+    i +=1
